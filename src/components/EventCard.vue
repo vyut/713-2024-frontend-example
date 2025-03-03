da36925192f1d9e6dae45ce222de3b89689b3512
Chartchai <chartchai.d@cmu.ac.th>
2025-03-04 06:03:09 +0700

    add list of objects

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
 import type { Event } from '@/types'
-import { ref } from 'vue'
 
-const event = ref<Event>({
-  id: 1,
-  category: 'Category 1',
-  title: 'Event 1',
-  description: 'Description of Event 1',
-  date: '2021-10-10',
-  time: '10:00',
-  location: 'Location 1',
-  petsAllowed: true,
-  organizer: {
-    name: 'Organizer 1',
-  },
-})
+defineProps<{ event: Event }>()
 </script>
 
 <template>
--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -2,15 +2,51 @@
 import { ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 const events = ref([
-  { id: 1, name: 'Event 1' },
-  { id: 2, name: 'Event 2' },
-  { id: 3, name: 'Event 3' },
+  {
+    id: 1,
+    category: 'Category 1',
+    title: 'Event 1',
+    description: 'Description of Event 1',
+    date: '2021-10-10',
+    time: '10:00',
+    location: 'Location 1',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 1',
+    },
+  },
+  {
+    id: 2,
+    category: 'Category 2',
+    title: 'Event 2',
+    description: 'Description of Event 2',
+    date: '2021-10-11',
+    time: '11:00',
+    location: 'Location 2',
+    petsAllowed: false,
+    organizer: {
+      name: 'Organizer 2',
+    },
+  },
+  {
+    id: 3,
+    category: 'Category 3',
+    title: 'Event 3',
+    description: 'Description of Event 3',
+    date: '2021-10-12',
+    time: '12:00',
+    location: 'Location 3',
+    petsAllowed: true,
+    organizer: {
+      name: 'Organizer 3',
+    },
+  },
 ])
 </script>
 
 <template>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 <style scoped>
