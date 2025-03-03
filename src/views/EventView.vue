35c90876146654aaf9b791fbdc9ca808b6857603
Chartchai <chartchai.d@cmu.ac.th>
2025-03-04 05:43:46 +0700

    with default to return the default value if the key is not found

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -10,6 +10,6 @@ const events = ref([
 
 <template>
   <div>
-    <EventCard v-for="event in events" :key="event.id" :name="event.name" />
+    <EventCard v-for="event in events" :key="event.id" />
   </div>
 </template>
