cfc2e5c958ac579588e1d1d81e665dc52e26f00a
Chartchai <chartchai.d@cmu.ac.th>
2025-03-03 11:30:15 +0700

    add file to make the empty presentation but menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventView from '../views/EventView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'events',
+      component: EventView,
     },
     {
       path: '/about',
