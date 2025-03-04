8dee757865cc48a3b506fe2d13d905392fdb6244
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 06:44:12 +0700

    add loading

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -4,9 +4,12 @@ import type { Event } from '@/types'
 const event = ref<Event>()
 </script>
 <template>
-  <div>
+  <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
   </div>
 </template>
