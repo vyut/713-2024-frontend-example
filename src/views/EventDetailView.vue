b4ead81c87ab35378340a66b6a4d23fd2d7c0128
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 06:41:15 +0700

    add empty events

---
+++ src/views/EventDetailView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types'
+const event = ref<Event>()
+</script>
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
