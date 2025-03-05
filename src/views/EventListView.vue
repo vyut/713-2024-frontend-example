bb211cb15faac3c6b32c7eebd93ea000cde93a39
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 14:56:40 +0700

    add naviagation button, cannot click next

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -19,6 +19,22 @@ eventService.getEvents(page.value, 2).then((response) => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <RouterLink
+        id="page-prev"
+        :to="{ name: 'event-list-view', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >Prev Page</RouterLink
+      >
+
+      <RouterLink
+        id="page-next"
+        :to="{ name: 'event-list-view', query: { page: page + 1 } }"
+        rel="next"
+        >Next Page</RouterLink
+      >
+    </div>
   </div>
 </template>
 <style scoped>
@@ -26,5 +42,23 @@ eventService.getEvents(page.value, 2).then((response) => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
