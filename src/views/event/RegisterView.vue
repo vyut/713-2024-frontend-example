49204520c3fe70e84eb5bcb985f50eb8bd93c90d
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 16:11:02 +0700

    use router push to send the other view name

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import type { Event } from '@/types'
+import { useRouter } from 'vue-router'
 const props = defineProps<{
   event: Event
   id: string
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
 const register = () => {
-  console.log('Register event here')
+  router.push({ name: 'event-detail-view' })
 }
 </script>
 <template>
