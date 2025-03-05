ad498b5c073033b00cfe74879ff52ad1f4c17954
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 17:20:15 +0700

    add nProgress to EventListView

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -4,6 +4,7 @@ import eventService from '@/services/EventService'
 import { ref, computed, watchEffect } from 'vue'
 import type { Event } from '@/types'
 import { useRouter } from 'vue-router'
+import nProgress from 'nprogress'
 const router = useRouter()
 const events = ref<Event[]>([])
 const totalEvents = ref(0)
@@ -20,11 +21,15 @@ watchEffect(() => {
   eventService
     .getEvents(page.value, 2)
     .then((response) => {
+      nProgress.start()
       events.value = response.data
       totalEvents.value = response.headers['x-total-count']
     })
     .catch(() => {
       router.push({ name: 'network-error-view' })
+    })
+    .finally(() => {
+      nProgress.done()
     })
 })
 eventService.getEvents(page.value, 2).then((response) => {
