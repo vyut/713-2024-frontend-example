e769e1546f8139c8cb5af5a358b5052119b3faa4
Chartchai <chartchai.d@cmu.ac.th>
2025-03-05 15:53:35 +0700

    fix to use sub route

--- src/views/event/DetailView.vue
+++ src/views/event/DetailView.vue
@@ -1,33 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { toRefs } from 'vue'
 import type { Event } from '@/types'
-import eventService from '@/services/EventService'
-const event = ref<Event>()
-const props = defineProps<{ id: string }>()
-const id = Number(props.id)
-eventService
-  .getEvent(id)
-  .then((response) => {
-    event.value = response.data
-  })
-  .catch((error) => {
-    console.error('There was an error!', error)
-  })
+const props = defineProps<{
+  event: Event
+  id: string
+}>()
+const { event } = toRefs(props)
 </script>
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <nav>
-      <router-link :to="{ name: 'event-detail-view', params: { id } }">Details</router-link>
-      |
-      <router-link :to="{ name: 'event-register-view', params: { id } }">Register</router-link>
-      |
-      <router-link :to="{ name: 'event-edit-view', params: { id } }">Edit</router-link>
-    </nav>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
-  </div>
-  <div v-else>
-    <p>Loading...</p>
-  </div>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
 </template>
---
+++ src/views/event/LayoutView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types'
+import eventService from '@/services/EventService'
+const event = ref<Event>()
+const props = defineProps<{ id: string }>()
+const id = Number(props.id)
+eventService
+  .getEvent(id)
+  .then((response) => {
+    event.value = response.data
+  })
+  .catch((error) => {
+    console.error('There was an error!', error)
+  })
+</script>
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <nav>
+      <router-link :to="{ name: 'event-detail-view' }">Details</router-link>
+      |
+      <router-link :to="{ name: 'event-register-view' }">Register</router-link>
+      |
+      <router-link :to="{ name: 'event-edit-view' }">Edit</router-link>
+    </nav>
+    <RouterView :event="event" />
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
