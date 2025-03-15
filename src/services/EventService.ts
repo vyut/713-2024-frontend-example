a07fe2b6bcd78bf0be368777769682d0d9741db5
vyut <vyut@msn.com>
2025-03-15 11:41:44 +0700

    Correct .env and eventService.ts

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
