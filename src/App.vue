a8f70f1c2047a6e8aef3ed8cec2edf2b61f9f376
vyut <vyut@msn.com>
2025-03-18 08:49:37 +0700

    Task 10 ข - การเพิ่ม Login & Logout Menu

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,81 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
+
+import SvgIcon from '@jamescoyle/vue-icon'
+// import { mdiAccountPlus } from '@mdi/js'
+// import { mdiLogin } from '@mdi/js';
+import { mdiAccountPlus, mdiAccount } from '@mdi/js'
+import { mdiLogin, mdiLogout } from '@mdi/js'
+import { useAuthStore } from './stores/auth'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
 const store = useMessageStore()
 const { message } = storeToRefs(store)
+
+function logout() {
+  authStore.logout()
+  router.push({ name: 'login' })
+}
 </script>
 
 <template>
-  <div id="layout">
-    <header>
-      <div id="flashMessage" v-if="message">
-        <h4>{{ message }}</h4>
-      </div>
-      <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-        </nav>
-      </div>
-    </header>
+<div id="layout">
+  <header>
+    <div id="flashMessage" v-if="message">
+      <h4>{{ message }}</h4>
+    </div>
 
-    <RouterView />
-  </div>
+    <nav class="flex">
+      <ul v-if="!authStore.currentUserName" class="flex navbar-nav ml-auto">
+        <li class="nav-item px-2">
+          <router-link to="/register" class="nav-link">
+            <div class="flex items-center">
+              <SvgIcon type="mdi" :path="mdiAccountPlus" /> <span class="ml-3">Sign Up</span>
+            </div>
+          </router-link>
+        </li>
+        <li class="nav-item px-2">
+          <router-link to="/login" class="nav-link">
+            <div class="flex items-center">
+              <SvgIcon type="mdi" :path="mdiLogin" /> <span class="ml-3">Login</span>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+      
+      <ul v-if="authStore.currentUserName" class="flex navbar-nav ml-auto">
+        <li class="nav-item px-2">
+          <router-link to="/profile" class="nav-link">
+            <div class="flex items-center">
+              <SvgIcon type="mdi" :path="mdiAccount" />
+              <span class="ml-3">{{ authStore.currentUserName }}</span>
+            </div>
+          </router-link>
+        </li>
+        <li class="nav-item px-2">
+          <a class="nav-link hover:cursor-pointer" @click="logout">
+            <div class="flex items-center">
+              <SvgIcon type="mdi" :path="mdiLogout" /> <span class="ml-3"> LogOut</span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+        <RouterLink :to="{ name: 'about' }">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  
+  <RouterView />
+</div>
 </template>
 
 <style>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,10 +1,18 @@
-import { defineStore } from 'pinia'
 import apiClient from '@/services/AxiosClient'
+import { defineStore } from 'pinia'
+import type { Organizer } from '@/types'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: null as string | null,
+    user: null as Organizer | null,
   }),
+  getters: {
+    currentUserName(): string {
+      return this.user?.username || ''
+    }
+  },
+
   actions: {
     async login(email: string, password: string) {
       const response = await apiClient.post('/api/v1/auth/authenticate', {
@@ -13,7 +21,20 @@ export const useAuthStore = defineStore('auth', {
       })
       this.token = response.data.access_token
       localStorage.setItem('token', this.token as string)
+      
+      const meResponse = await apiClient.get('/api/v1/auth/me')
+      this.user = meResponse.data.user
+      localStorage.setItem('user', JSON.stringify(this.user))
+
       return response
     },
+    logout() {
+      console.log('logout')
+      this.token = null
+      this.user = null
+      localStorage.removeItem('access_token')
+      localStorage.removeItem('user')
+    },
+
   },
 })
